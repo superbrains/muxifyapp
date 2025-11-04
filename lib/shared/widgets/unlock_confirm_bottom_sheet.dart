a3af6062887e3df4e620380a5464ea5539a69f75
Habedexise1 <alexadepetu@gmail.com>
2025-11-04 22:13:32 +0100

    Update Podfile.lock to include local_auth_darwin dependency and remove unused import in unlock_confirm_bottom_sheet.dart

--- lib/shared/widgets/unlock_confirm_bottom_sheet.dart
+++ lib/shared/widgets/unlock_confirm_bottom_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:muxify/core/constants/app_colors.dart';
 import 'package:muxify/core/constants/app_sizes.dart';
 import 'package:muxify/core/constants/app_text_styles.dart';
